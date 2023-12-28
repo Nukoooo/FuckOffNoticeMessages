c6620f1c5c09341a635676bc973e03d1c2c9b125
Nukoooo <me@nuko.me>
2023-12-28 09:51:58 +0800

    A

---
+++ FuckOffNoticeMessages/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+
+namespace FuckOffNoticeMessages;
+
+public class Plugin : IDalamudPlugin
+{
+    public Plugin([RequiredVersion("1.0")] DalamudPluginInterface pi)
+    {
+        pi.Create<Service>();
+
+        Service.ChatGui.ChatMessage += ChatGui_OnChatMessage;
+    }
+
+    private void ChatGui_OnChatMessage(XivChatType type, uint senderid, ref SeString sender, ref SeString message, ref bool ishandled)
+    {
+        if (type != XivChatType.Notice)
+            return;
+
+        ishandled = true;
+    }
+
+    public string Name => "FuckOffNoticeMessages";
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposing)
+            return;
+        Service.ChatGui.ChatMessage -= ChatGui_OnChatMessage;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ FuckOffNoticeMessages/Service.cs
@@ -0,0 +1,9 @@
+﻿using Dalamud.Game.Gui;
+using Dalamud.IoC;
+
+namespace FuckOffNoticeMessages;
+
+internal class Service
+{
+    [PluginService] internal static ChatGui ChatGui { get; private set; } = null!;
+}
